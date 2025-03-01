bd221344cb0f491dc097f789e1d06cd24b208512
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-03-02 01:22:09 +0530

    Update header.h

--- Assignment 1 (Home Assignment)/header.h
+++ Assignment 1 (Home Assignment)/header.h
@@ -1,31 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 #define MAX 512
+#define KEY ftok("memory", 67)
 
-typedef struct
-{
-	long int ttype;
-	int tot;
-}total;
-
-typedef struct
-{
-	long int mtype1;
-	char data[BUFSIZ];
-}messageName;
-
-typedef struct
-{
-	long int mtype2;
-	int num;
-}messageRoll;
-
+typedef struct { long ttype; int tot; } total;
+typedef struct { long mtype; char data[MAX]; } message;
+typedef struct { long mtype; int num; } messageRoll;
