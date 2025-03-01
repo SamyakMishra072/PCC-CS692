0f486247a86280df40d76fb083417d9d94e79614
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-03-02 01:23:02 +0530

    Update process3.c

--- Assignment 1 (Home Assignment)/process3.c
+++ Assignment 1 (Home Assignment)/process3.c
@@ -1,79 +1,28 @@
 #include "header.h"
 
-int compare(const void *a, const void *b)
-{
-	return (*(int *)a - *(int *)b);
-}
+int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
 
-void main()
-{
-	int n=0,i=0,j;
-	messageRoll mroll,mrollreturn;
-	total tot;
+int main() {
+    int n, i, msgid = msgget(KEY, 0666 | IPC_CREAT);
+    total tot; messageRoll mroll = {2}, mrollreturn = {6};
+    int *rolls;
 
-	int msgid;
-	char bufname[BUFSIZ];
-	int *rolln;
-	long int mp=2;
-	key_t key=ftok("memory",67);
-	msgid=msgget(key,0666|IPC_CREAT);
-	
-	if(msgid==-1)
-	{
-		printf("Cannot connect the message queue\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	msgrcv(msgid,(void *)&tot,BUFSIZ,4,0);
-	n=tot.tot;
+    msgrcv(msgid, &tot, sizeof(total), 4, 0);
+    n = tot.tot;
+    rolls = malloc(n * sizeof(int));
 
-	rolln=(int *)malloc(n*sizeof(int));
+    for (i = 0; i < n; i++) {
+        msgrcv(msgid, &mroll, sizeof(mroll), 2, 0);
+        rolls[i] = mroll.num;
+    }
 
-	for(j=0;j<n;j++)
-	{
-		if(msgrcv(msgid,(void *)&mroll,BUFSIZ,mp,0)==-1)
-		{
-			printf("Cannot read roll %d\n",j+1);
-		}
+    qsort(rolls, n, sizeof(int), compare);
 
-		printf("Received Roll %d : %d\n",j+1,mroll.num);
-		rolln[j]=mroll.num;
-	}
-	
-	//printf("%d %d",n,i);
-
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n",rolln[i]);
-	}
-	
-	printf("\n");
-
-	qsort(rolln,n,sizeof(int), compare);
-
-	printf("After sorting\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n",rolln[i]);
-	}
-
-	printf("\n");
-	for(i=0;i<n;i++)
-	{
-		mrollreturn.mtype2=6;
-		mrollreturn.num=rolln[i];
-		if(msgsnd(msgid,(void *)&mrollreturn,MAX,0)==-1)
-		{
-			printf("Cannot send back roll\n");
-			exit(1);
-		}
-		else
-		{
-			printf("%d sent back\n",rolln[i]);
-		}
-	}
-
-	exit(EXIT_SUCCESS);
-	
-	
+    for (i = 0; i < n; i++) {
+        mrollreturn.num = rolls[i];
+        msgsnd(msgid, &mrollreturn, sizeof(mrollreturn), 0);
+    }
+    
+    free(rolls);
+    return 0;
 }
