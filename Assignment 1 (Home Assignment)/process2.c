39598b364ab09fef9f96eb259ed75588566b2e46
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-03-02 01:22:45 +0530

    Update process2.c

--- Assignment 1 (Home Assignment)/process2.c
+++ Assignment 1 (Home Assignment)/process2.c
@@ -1,79 +1,29 @@
 #include "header.h"
 
-int compare(const void *a, const void *b)
-{
-	return strcmp(*(const char **)a, *(const char **)b);
-}
+int compare(const void *a, const void *b) { return strcmp(*(char **)a, *(char **)b); }
 
-void main()
-{
-	int n=0,i=0,j;
-	messageName mname,mnamereturn;
-	total tot;
+int main() {
+    int n, i, msgid = msgget(KEY, 0666 | IPC_CREAT);
+    total tot; message mname = {1}, mnamereturn = {5};
+    char **names;
 
-	int msgid;
-	char bufname[BUFSIZ],**string;
-	long int mp=1;
-	key_t key=ftok("memory",67);
-	msgid=msgget(key,0666|IPC_CREAT);
-	
-	if(msgid==-1)
-	{
-		printf("Cannot connect the message queue\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	msgrcv(msgid,(void *)&tot,BUFSIZ,3,0);
-	n=tot.tot;
+    msgrcv(msgid, &tot, sizeof(total), 3, 0);
+    n = tot.tot;
+    names = malloc(n * sizeof(char *));
+    
+    for (i = 0; i < n; i++) {
+        msgrcv(msgid, &mname, sizeof(mname), 1, 0);
+        names[i] = strdup(mname.data);
+    }
 
-	string=(char **)malloc(n*sizeof(char *));
+    qsort(names, n, sizeof(char *), compare);
 
-	for(j=0;j<n;j++)
-	{
-		if(msgrcv(msgid,(void *)&mname,BUFSIZ,mp,0)==-1)
-		{
-			printf("Cannot read name %d\n",j+1);
-		}
-
-		printf("Received Name %d : %s\n",j+1,mname.data);
-		string[j]=(char *)malloc(MAX*sizeof(char));
-		strcpy(string[j],mname.data);
-	}
-	
-	//printf("%d %d",n,i);
-
-	for(i=0;i<n;i++)
-	{
-		printf("%s\n",string[i]);
-	}
-	
-	printf("\n");
-
-	qsort(string,n,sizeof(char *),compare);
-
-	printf("After sorting\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%s\n",string[i]);
-	}
-	
-	printf("\n");
-	for(i=0;i<n;i++)
-	{
-		mnamereturn.mtype1=5;
-		strcpy(mnamereturn.data,string[i]);
-		if(msgsnd(msgid,(void *)&mnamereturn,MAX,0)==-1)
-		{
-			printf("Cannot send back data\n");
-			exit(1);
-		}
-		else
-		{
-			printf("%s sent back\n",string[i]);
-		}
-	}
-
-	exit(EXIT_SUCCESS);
-	
-	
+    for (i = 0; i < n; i++) {
+        strcpy(mnamereturn.data, names[i]);
+        msgsnd(msgid, &mnamereturn, sizeof(mnamereturn), 0);
+        free(names[i]);
+    }
+    
+    free(names);
+    return 0;
 }
