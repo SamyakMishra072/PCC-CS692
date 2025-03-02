01f0c89025921aea2c1bebc73e3eb7e3d7abd581
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-03-02 11:44:20 +0530

    Update header.h

--- Assignment 1 (Home Assignment)/header.h
+++ Assignment 1 (Home Assignment)/header.h
@@ -4,16 +4,18 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-
 #define MAX 512
 #define KEY ftok("memory", 67)
 
 typedef struct { 
-  long ttype; int tot; 
+  long ttype;
+  int tot; 
 } total;
 typedef struct {  
-  long mtype; char data[MAX]; 
+  long mtype;
+  char data[MAX]; 
 } message;
 typedef struct { 
-  long mtype; int num; 
+  long mtype;
+  int num; 
 } messageRoll;
