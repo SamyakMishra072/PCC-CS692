45ecf3563a0ebba906db33d9f48c6b50b97e5166
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-03-02 11:43:22 +0530

    Update process3.c

--- Assignment 1 (Home Assignment)/process3.c
+++ Assignment 1 (Home Assignment)/process3.c
@@ -1,28 +1,25 @@
 #include "header.h"
-
-int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
-
+int compare (const void *a, const void *b) { 
+    return (*(int *)a - *(int *)b);
+}
 int main() {
     int n, i, msgid = msgget(KEY, 0666 | IPC_CREAT);
-    total tot; messageRoll mroll = {2}, mrollreturn = {6};
+    total tot; 
+    messageRoll mroll = {2};
+    mrollreturn = {6};
     int *rolls;
-
     msgrcv(msgid, &tot, sizeof(total), 4, 0);
     n = tot.tot;
     rolls = malloc(n * sizeof(int));
-
     for (i = 0; i < n; i++) {
         msgrcv(msgid, &mroll, sizeof(mroll), 2, 0);
         rolls[i] = mroll.num;
     }
-
     qsort(rolls, n, sizeof(int), compare);
-
     for (i = 0; i < n; i++) {
         mrollreturn.num = rolls[i];
         msgsnd(msgid, &mrollreturn, sizeof(mrollreturn), 0);
     }
-    
     free(rolls);
     return 0;
 }
